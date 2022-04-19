576d2113e810ff906665b59f63a4f927e070bead
quynguyen919 <quy.ndm9039@sinhvien.hoasen.edu.vn>
2022-04-19 07:55:12 +0700

    project nhóm

---
+++ Kenh14_csharp/Item.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kenh14_csharp
+{
+    public class Item
+    {
+        //properties
+        public string Name
+        {
+            get; set;
+        }
+        public string Accessories
+        {
+            get; set;
+        }
+        public string PhotoURL
+        {
+            get; set;
+        }
+        public string Price
+        {
+            get; set;
+        }
+
+        // constructor
+        public Item()
+        {
+
+        }
+        //methods
+
+
+    }
+}
